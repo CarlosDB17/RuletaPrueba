ba4c3fbdf4f602c707584037165e0f5b5b6b41f9
CarlosDB17 <carlosdbtic@gmail.com>
2025-04-17 11:08:36 +0200

    feat: mejorar la retroalimentación del giro de la ruleta con mensajes de consola

--- src/app/components/ruleta/ruleta.component.ts
+++ src/app/components/ruleta/ruleta.component.ts
@@ -33,13 +33,24 @@ export class RuletaComponent implements OnInit {
     const totalPasos = vueltas * this.gajos.length + target;
     let pasos = 0;
 
+    console.log(' Iniciando giro de la ruleta...');
+    
     const intervalo = setInterval(() => {
       this.selectorIndex = (this.selectorIndex + 1) % this.gajos.length;
+      const gajoActual = this.gajos[this.selectorIndex];
+      console.log(`Girando: ${gajoActual.titulo} - Color: ${gajoActual.color}`);
       pasos++;
       if (pasos >= totalPasos) {
         clearInterval(intervalo);
         this.premioIndex = this.selectorIndex;
         this.girando = false;
+        const gajoGanador = this.gajos[this.premioIndex];
+        console.log(`¡RESULTADO FINAL! - ${gajoGanador.titulo} - Color: ${gajoGanador.color} - Tipo: ${gajoGanador.tipo}`);
+        if (gajoGanador.tipo === 'premio') {
+          console.log('¡FELICIDADES! Has ganado un premio');
+        } else {
+          console.log(' Lo siento, no has ganado esta vez');
+        }
       }
     }, 100);
   }
